4ef443e2631dd58414d173c857223b9c83191aa5
Derek Chen <derex1987@gmail.com>
2025-09-11 00:32:02 -0500

    feat: spawn detonator given player count

--- src/main/java/io/github/derec4/bowsersBigBlast/game/DetonatorManager.java
+++ src/main/java/io/github/derec4/bowsersBigBlast/game/DetonatorManager.java
@@ -4,12 +4,10 @@ import io.github.derec4.bowsersBigBlast.plunger.Detonator;
 import io.github.derec4.bowsersBigBlast.plunger.DetonatorLocation;
 import io.github.derec4.bowsersBigBlast.util.BlockUtils;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.type.Button;
+import org.bukkit.block.Block;
 import org.bukkit.util.Vector;
 import java.util.*;
 
@@ -32,29 +30,29 @@ public class DetonatorManager {
         List<Material> wools = new ArrayList<>(BlockUtils.WOOLS);
         Collections.shuffle(wools);
         List<Material> usedWools = wools.subList(0, Math.min(playerCount, wools.size()));
+
         Random rand = new Random();
 
+        // spawn detonators, centered at the player, 2 blocks in front of them, separated by 1 gap
         for (int i = 0; i < playerCount; i++) {
             Material woolType = usedWools.get(i);
             Material buttonType = BlockUtils.BUTTONS.get(rand.nextInt(BlockUtils.BUTTONS.size()));
 
             // Calculate location: 1 block ahead per detonator, at player's Y
-            Location woolLoc = base.clone().add(direction.clone().multiply(i + 2)).getBlock().getLocation();
+            Location woolLoc = base.clone().add(direction.clone().multiply(i + 2));
             woolLoc.setY(base.getBlockY());
-            Block woolBlock = world.getBlockAt(woolLoc);
-            woolBlock.setType(woolType);
+            BlockUtils.placeDetonatorBlock(woolLoc, woolType, buttonType);
 
-            // Button on top
+            // Get the button block location
             Location buttonLoc = woolLoc.clone().add(0, 1, 0);
             Block buttonBlock = world.getBlockAt(buttonLoc);
-            buttonBlock.setType(buttonType);
-            BlockData data = buttonBlock.getBlockData();
-            if (data instanceof Button) {
-                ((Button) data).setFace(org.bukkit.block.BlockFace.FLOOR);
-                buttonBlock.setBlockData(data);
-            }
 
-            DetonatorLocation detLoc = new DetonatorLocation(woolLoc);
+            DetonatorLocation detLoc = new DetonatorLocation(
+                buttonBlock.getWorld().getName(),
+                buttonBlock.getX(),
+                buttonBlock.getY(),
+                buttonBlock.getZ()
+            );
             Detonator detonator = new Detonator(detLoc, buttonBlock, true);
             detonators.add(detonator);
         }
